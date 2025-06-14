7847840483dcc51451d96647fc5b3092c5bd659d
TelevisionNinja <TelevisionNinja@gmail.com>
2025-06-14 03:55:05 -0400

    update data access layer

--- src/lib/dal.js
+++ src/lib/dal.js
@@ -1,30 +1,23 @@
 import 'server-only';
 
 import { cookies } from 'next/headers';
-import { decrypt, COOKIE_SESSION_KEY } from './cookieManagement';
-import { getUser, getSession } from "./database";
+import { getUserFromSession } from "./session";
 import { cache } from "react";
 import { redirect } from "next/navigation";
 
+/**
+ * role = null, allow any signed in user
+ * role = string, allow only users with that role
+ */
 export const verifySession = cache(async (role = null) => {
-    const cookie = (await cookies()).get(COOKIE_SESSION_KEY)?.value;
-    const cookieInfo = await decrypt(cookie);
+    const user = await getUserFromSession(await cookies());
 
-    if (!cookieInfo) {
+    if (!user) {
         redirect('/sign-in');
         return null;
     }
 
-    const session = getSession(cookieInfo.sessionId);
-
-    if (!session) {
-        redirect('/sign-in');
-        return null;
-    }
-
-    const user = getUser(session.email);
-
-    if (user.role === role) {
+    if (role === null || user.role === role) {
         return user;
     }
 
